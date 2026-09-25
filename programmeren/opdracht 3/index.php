426f370d414c0bef615debcec49e6b9d845fd710
simionvw <61330022+simionvw@users.noreply.github.com>
2026-09-25 15:13:40 +0200

    a

---
+++ programmeren/opdracht 3/index.php
@@ -0,0 +1,16 @@
+<?php 
+  class Figure {
+    public string $color;
+
+    function __construct($color)
+    {
+      $this->color = $color;
+    }
+  }
+
+
+
+
+  echo  '<svg><rect width="100" height="100" fill="blue" /></svg>';
+
+?>
